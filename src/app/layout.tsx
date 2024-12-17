1c59df0b2a0527bb6a5c2f41c101a50b7364dfbd
coder-dom <giangtruong100702@gmail.com>
2024-12-17 12:41:34 +0700

    KHỞI TẠO DỰ ÁN MUSIC APPCLOUD

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Music AppCloud",
+  description: "Ứng dụng nghe nhạc trực tuyến",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+// Metadata
+export const metadata: Metadata = {
+  title: "Music AppCloud",
+  description: "Ứng dụng nghe nhạc trực tuyến",
+};
+
+export default function Home() {
+  return (
+    <>
+
+    </>
+  );
+}
