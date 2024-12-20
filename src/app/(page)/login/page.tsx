e165b281d9d764a7fe6ab36d305a665cc78c0344
coder-dom <giangtruong100702@gmail.com>
2024-12-20 20:53:08 +0700

    Trang Đăng Ký Tài Khoản

--- src/app/(page)/login/page.tsx
+++ src/app/(page)/login/page.tsx
@@ -1,6 +1,8 @@
+// components
 import { BoxTitle } from "@/app/components/BoxTitle/BoxTitle";
 import { FormButtonSubmit } from "@/app/components/Form/FormButtonSubmit";
 import { FormInput } from "@/app/components/Form/FormInput";
+
 import type { Metadata } from "next";
 
 // Metadata
@@ -10,26 +12,26 @@ export const metadata: Metadata = {
 };
 
 export default function Login() {
-    return (
-        <>
-          <div className="w-full h-full flex items-center justify-center">
-              {/* form đăng nhập */}
-              <form 
-                action=""
-                className="w-[500px]"
-              >
-                {/* title */}
-                <div className="text-center">
-                  <BoxTitle title="Đăng Nhập Tài Khoản" className="" />
-                </div>
+  return (
+      <>
+        <div className="w-full h-full flex items-center justify-center">
+            {/* form đăng nhập */}
+            <form 
+              action=""
+              className="w-[500px]"
+            >
+              {/* title */}
+              <div className="text-center">
+                <BoxTitle title="Đăng Nhập Tài Khoản" className="" />
+              </div>
 
-                <FormInput type="email" placeholder="Ví dụ: levana@gmail.com"/>
-                <FormInput type="password" />
-                <FormButtonSubmit text="Đăng Nhập" />
+              <FormInput type="email" placeholder="Ví dụ: levana@gmail.com"/>
+              <FormInput type="password" />
+              <FormButtonSubmit text="Đăng Nhập" />
 
-              </form>
-          </div>
+            </form>
+        </div>
 
-        </>
-      );
+      </>
+    );
 }
--- src/app/(page)/register/page.tsx
+++ src/app/(page)/register/page.tsx
@@ -6,10 +6,33 @@ export const metadata: Metadata = {
   description: "Ứng dụng nghe nhạc trực tuyến",
 };
 
+// components
+import { BoxTitle } from "@/app/components/BoxTitle/BoxTitle";
+import { FormButtonSubmit } from "@/app/components/Form/FormButtonSubmit";
+import { FormInput } from "@/app/components/Form/FormInput";
+
 export default function Register() {
-    return (
-        <>
-          <h1>TRANG DANH ĐĂNG KÝ</h1>
-        </>
-      );
+  return (
+      <>
+        <div className="w-full h-full flex items-center justify-center">
+            {/* form đăng nhập */}
+            <form 
+              action=""
+              className="w-[500px]"
+            >
+              {/* title */}
+              <div className="text-center">
+                <BoxTitle title="Đăng Ký Tài Khoản" className="" />
+              </div>
+
+              <FormInput type="text" placeholder="Ví dụ: Le Van A"/>
+              <FormInput type="email" placeholder="Ví dụ: levana@gmail.com"/>
+              <FormInput type="password" />
+              <FormButtonSubmit text="Đăng Ký" />
+
+            </form>
+        </div>
+
+      </>
+  );
 }
