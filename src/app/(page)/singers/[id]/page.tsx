f002345f0c3a0e247479163d5cfc4de973ab8f76
coder-dom <giangtruong100702@gmail.com>
2024-12-20 20:01:01 +0700

    Trang Chi Tiết Ca Sĩ

--- src/app/(page)/singers/[id]/page.tsx
+++ src/app/(page)/singers/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { BoxTitle } from "@/app/components/BoxTitle/BoxTitle";
+import { CardInfo } from "@/app/components/Card/CardInfo";
+import { Song2 } from "@/app/components/Song2/Song2";
 import type { Metadata } from "next";
 
 // Metadata
@@ -7,9 +10,22 @@ export const metadata: Metadata = {
 };
 
 export default function SingerDetailPage() {
+
+    // thông tin ca sĩ
+    const infoCard = {
+      image: '/demo/sontung-mtp.png',
+      title: 'Sơn Tùng M-TP',
+      desc:  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    }
+
     return (
         <>
-          <h1>TRANG CHI TIẾT CA SĨ</h1>
+          <CardInfo info={infoCard} />
+
+          {/* Danh sách bài hát */}
+          <BoxTitle title="Danh Sách Bài Hát" className="" />
+
+          <Song2 />
         </>
       );
 }
