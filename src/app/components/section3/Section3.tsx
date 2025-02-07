66e48d2d6c925b93bc538aeff12e6c1fc29303ea
coder-dom <giangtruong100702@gmail.com>
2025-02-08 01:41:07 +0700

    cập nhật + fetch lại data section 3 TRANG CHỦ - CA SĨ NỔI BẬT

--- src/app/components/section3/Section3.tsx
+++ src/app/components/section3/Section3.tsx
@@ -1,48 +1,53 @@
+"use client";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // components
 import { BoxTitle } from "../BoxTitle/BoxTitle";
 import { Card } from '../Card/Card';
 
+// hook
+import { useEffect, useState } from "react";
+
 // firebase
-import { ref, onValue } from "firebase/database";
+import { ref, get } from "firebase/database";
 
 // firebase config
 import { Database } from '../../config/firebaseConfig';
 
-// interface
-import * as CategoryInterface from '../../interfaces/categories'; 
-import * as SingerInterface from '../../interfaces/singer';
+// reload raw data
+const reloadRawData = (rawData: any) => {
+    const data: any[] = [];
+    rawData.forEach((item: { key: any; val: () => any; }) => { 
+        data.push({ id: item.key, ...item.val() }) 
+    })
+    return data;
+}
 
-// Intersection type (hợp nhiều interface làm 1), dùng dấu &
-type GeneralInterface = CategoryInterface.Category & SingerInterface.Singer;
+// Lấy danh sách danh mục
+const getListSong = async() => {
+     // lấy dữ liệu raw trên firebase
+     const rawData = await get(ref(Database, 'singers'));
+     const data: any[] = reloadRawData(rawData);
 
-
-// collection lấy dữ liệu từ firebase
-const singerRef = ref(Database, 'singers'); 
+     return data;
+}
 
 export const Section3 = () => {
-        // mảng chứa data 
-        const DataSection: GeneralInterface[] = [];
-        
-        onValue(singerRef, (singerItems) => {
-            singerItems.forEach( singerItem => {
-                const categoryId   = singerItem.key;
-                const categoryData = singerItem.val();
-                
-                const item: GeneralInterface = {
-                    id: categoryId,
-                    title: categoryData.title,
-                    description: categoryData.description,
-                    image: categoryData.image,
-                };
+    const [songs, setSongs] = useState<any>(null);
+    
+    useEffect(() => {
+        const fetchOnFirebase = async () => {
+            const data: any[] = await getListSong();
+            setSongs(data);
+        }
 
-                if(DataSection.length < 5 ) DataSection.push(item);
-            })
-        });
+        fetchOnFirebase();
+    }, [songs]);
 
     return (
         <>
             <BoxTitle title="Ca sĩ nổi bật"  className=""/>
-            <Card data = {DataSection} linkTo = {'/singers'} />     
+            <Card data = {songs} linkTo = {'/singers'} />     
         </>
     );
 }
